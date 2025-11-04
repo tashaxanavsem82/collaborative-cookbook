c411b7674d5e05c9ddc686807b0a07dcdfe5337a
tashaxanavsem82 <swgwull@hotmail.com>
2025-11-04 05:08:05 -0600

    Update mongoose connection options to remove deprecated useCreateIndex

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connection established.'))
     .catch(err => console.log(err));
 
