11bd18b6a04962efc8517f8efd9d468f1ca71a9c
tashaxanavsem82 <swgwull@hotmail.com>
2025-11-04 05:07:51 -0600

    Update MongoDB connection options for deprecation warnings

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then(() => console.log('MongoDB connection established.'))
     .catch(err => console.log(err));
 
