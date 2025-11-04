6f40f79045378100d4a2c6c9c0019a18b07c8de8
tashaxanavsem82 <swgwull@hotmail.com>
2025-11-04 05:07:37 -0600

    Refactor error handling middleware to return a JSON response.

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 // Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).send('Something broke!');
+    res.status(500).json({ message: 'Something broke!' });
 });
 
 app.listen(PORT, () => {
